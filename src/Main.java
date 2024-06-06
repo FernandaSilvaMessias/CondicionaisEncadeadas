36fa24733ca0a69bcb5abae4fee7715d8d87db1f
CassiaFernandaMessias <cassia.fernanda.dev@gmail.com>
2024-06-06 15:37:57 -0300

    DIO

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int nota = 8;
+
+        if (nota >= 7) {
+            System.out.println("Aprovado");
+        }
+       else if (nota >= 5 && nota < 7) {
+        }
+           else
+               System.out.println("Reprovado");
+        }
+
+
+    }
